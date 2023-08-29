37573bb868d1b7f5195a45305446a3becfcffe59
Iván <ivantbd87@gmail.com>
2023-08-29 16:31:32 +0200

    fix: homeScreen styles

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -13,11 +13,25 @@ function HomeScreen() {
   console.log(background)
   return (
     <>
-    HOme
-    <Link to={'/pokemons'} className="btn btn-lg btn-secondary" >view pokemons</Link>
-      <button onClick={() => setCharmander()} className="btn btn-danger">Charmander</button>
-      <button onClick={() => setSquirtle()} className="btn btn-info">Squirtle</button>
-      <button onClick={() => setBulbasaur()} className="btn btn-success">Bulbasaur</button>
+    <h3 className="pt-3 text-center">Elige un pokemon</h3>
+    <div className="d-flex justify-content-evenly flex-wrap">
+      <div className="d-flex flex-column">
+        <img className="pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" alt="" />
+        <button onClick={() => setCharmander()} className="btn btn-danger">Charmander</button>
+      </div>
+      <div className="d-flex flex-column">
+        <img className="pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt="" />
+        <button onClick={() => setSquirtle()} className="btn btn-info">Squirtle</button>
+      </div>
+      <div className="d-flex flex-column">
+        <img className="pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" />
+        <button onClick={() => setBulbasaur()} className="btn btn-success">Bulbasaur</button>
+      </div>
+    </div>
+    <h1 className="mt-5 text-center">Y ahora...</h1>
+    <div className="d-flex mt-3">
+      <Link to={'/pokemons'} className="btn btn-lg btn-outline-secondary mx-auto" >view pokemons</Link>
+    </div>
     </>
   )
 }
